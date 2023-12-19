da2da6a29d9e7e4717958412df7941d404e6aa5e
Rogério <rogeriobritoufu@gmail.com>
2023-12-18 22:57:48 -0300

    Implement get book by id (gin)

--- src/infra/controller/gin_controller_adapter.go
+++ src/infra/controller/gin_controller_adapter.go
@@ -24,33 +24,36 @@ func (gc GinControllerAdapter) CreateBookUseCase(c *gin.Context) {
 
 	b, err := gc.Controller.CreateBookUseCase(request)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, "")
+		c.String(http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	c.JSON(http.StatusOK, b)
 }
 
 // adapts to GetBook controller function
-func (gc GinControllerAdapter) GetBook(c *gin.Context) error {
+func (gc GinControllerAdapter) GetBook(c *gin.Context) {
 	body, _ := ioutil.ReadAll(c.Request.Body)
 	request := Request{
-		pathVariables: nil, // TODO: parse path variables
-		params:        nil, // TODO: parse params
-		headers:       nil, // TODO: parse headers
-		body:          body,
+		pathVariables: map[string]string{
+			"id": c.Param("id"),
+		},
+		params:  nil, // TODO: parse params
+		headers: nil, // TODO: parse headers
+		body:    body,
 	}
 
 	b, err := gc.Controller.GetBook(request)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, "")
+		c.String(http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	c.JSON(http.StatusOK, b)
-	return nil
 }
 
 func (gc GinControllerAdapter) Start() error {
-	gc.Gin.GET("/book", func(c *gin.Context) {
+	gc.Gin.GET("/book/:id", func(c *gin.Context) {
 		gc.GetBook(c)
 	})
 
